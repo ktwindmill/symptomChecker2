d9be4365fcc71ac4230a49c9d191bc553fe54844
Katie <ktind001@gold.ac.uk>
2021-04-19 18:00:20 +0100

    initial commit

---
+++ sketch.js
@@ -0,0 +1,224 @@
+/*
+Data and machine learning for artistic practice
+Week 8
+
+Stateful generation CharRNN
+*/
+
+let buttonArray = [];
+let string = '';
+let reset;
+let submit;
+
+//button positions
+var radius;
+var angle;
+var step;
+
+//strings/story
+let title = "What's ailing you?";
+
+//charRNN variables
+let charRNN, 
+    isGenerating = false,
+    generated_text ='',
+prediction_text = '';
+let story='';
+
+//images
+let backgroundImage;
+
+function preload(){
+
+  westernFont = loadFont('media/font/Carnevalee Freakshow.ttf');
+    roboto = loadFont('media/font/Roboto-Bold.ttf');
+    
+    backgroundImage=loadImage('media/layout.jpg');
+}
+
+
+
+function setup() {
+  createCanvas(500, 900);
+  
+  // Create the LSTM Generator passing it the model directory
+  charRNN = ml5.charRNN('./models/adages/', modelReady);
+  
+  
+     
+    //alphabet
+    background(0);
+  
+  
+  //variables for the button positions
+  r = 180;
+  angle = 0;
+  
+  //pi * 2
+  step = 6.28318/26;
+  
+ 
+  
+  //create the alphabet
+for(let i = 1; i<27; i++){
+  let letter = char(64+i);
+  
+  buttonArray.push(createButton(letter, letter));
+  
+  
+}  
+
+  
+ 
+  //give the alphabet a function and a position in a circle
+  for(let i = 0; i<buttonArray.length; i++){
+    
+    buttonArray[i].mousePressed(changeBG);
+    var x = r *sin(angle + (step*i))+(width/2);
+    var y = r*cos(angle+ (step*i))+(400);
+    buttonArray[i].style("font-family","Times");
+      buttonArray[i].style("background-color",'#ffffff');
+      buttonArray[i].style("color","#2ed93b");
+      buttonArray[i].position(x,y);
+      
+  }
+  
+  submit = createButton('SUBMIT');
+  submit.mousePressed(generate);
+    submit.position(width/2-60,height-80);
+   submit.style("font-family","Times");
+      submit.style("background-color",'#ffffff');
+    submit.style("color","#fcba03");
+    
+    
+    
+    reset = createButton('RESET');
+  reset.mousePressed(resetString);
+  reset.position(width/2+25,height-80);
+     reset.style("font-family","Times");
+      reset.style("background-color",'#ffffff');
+    reset.style("color","#Gfcba03");
+        
+
+  
+    
+    
+}
+
+function draw() {
+    
+ background(0);
+  image(backgroundImage,0,0,width,height);
+    
+    
+    fill(255);
+  textFont(westernFont);
+  textSize(36);
+  textAlign(CENTER, CENTER);
+  text(title, 40,0,width-80,150);
+  
+    
+    
+  fill(255, 232, 25);  
+  textSize(40);
+  textAlign(CENTER, CENTER);
+  text(string, width/2-60,220,130,300);
+    
+     fill(255);  
+textSize(20);
+    text(story, 30, 690, width-60, 100);
+    
+    textFont(roboto);
+    textSize(12);
+    textAlign(LEFT);
+    text("This text was generated using", 20, height-40);
+    
+
+}
+
+
+
+function changeBG() {
+  let letter = this.value();
+  letter = letter.toLowerCase();
+  //console.log(letter);
+  string = string+letter;
+  //console.log(string)
+}
+
+function resetString(){
+      background(0);
+    string='';
+    // Clear everything
+      prediction_text = "";
+      original_text = "";
+    story ='';
+    //background(0);
+    resetModel();
+    
+}
+
+
+
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, canvasHeight);
+}
+
+async function modelReady() {
+  console.log('model ready')
+  resetModel();
+}
+
+function resetModel() {
+  charRNN.reset();
+  const seed = string;
+  charRNN.feed(seed);
+  generated_text = seed;
+}
+
+function generate() {
+    if(!isGenerating) {
+    isGenerating = true;
+    
+        console.log('generating...');
+    
+    // Grab the original text
+    let original = string;
+    original_text = original;
+
+    
+    // Check if there's something
+    if (original_text.length > 0) {
+        
+        let newString = ` ${original_text} can be treated by `;
+      // Here is the data for the LSTM generator
+      let data = {
+       // seed: original_text,
+        seed: newString,
+          temperature: 0.60,
+        length:120
+      };
+    
+      // Generate text with the charRNN
+      charRNN.generate(data, gotData);
+    
+    }else {
+      // Clear everything
+      prediction_text = "";
+      original_text = "";
+         story ='';
+    }
+  }
+}
+
+// Update the DOM elements with typed and generated text
+function gotData(err, result) {
+ console.log('ahem');
+  prediction_text = result.sample;
+    story = ` ${string} can be treated by ${prediction_text}`
+  isGenerating = false;
+
+}
+
